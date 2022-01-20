1c4b5793850a98ba439b2e9ac4c1ee11f401560a
DiegoVega19 <davila9923@gmail.com>
2022-01-19 19:19:44 -0600

    commit inicial

---
+++ ApiRest/Controllers/LugaresController.cs
@@ -0,0 +1,33 @@
+﻿using Core.Entities;
+using Infraestructura.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LugaresController : ControllerBase
+    {
+        private readonly AppDBContext _db;
+        public LugaresController(AppDBContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Lugar>>> getLugares()
+        {
+            var places = await _db.Lugar.ToListAsync();
+            return Ok(places);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Lugar>> getLugar(int id)
+        {
+            var place = await _db.Lugar.FindAsync(id);
+            return Ok(place);   
+        }
+    }
+}
---
+++ Infraestructura/Data/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infraestructura.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet <Lugar> Lugar { get; set; }
+    }
+}
